d8fb0363759fa537827fe32bb759fb2e4625a407
DESKTOP-H39OAJ3\danielpumayauli <danielpumayauli@gmail.com>
2017-02-10 18:52:58 -0500

    Update

---
+++ src/java/com/escueladigital/dao/ProductoDAO.java
@@ -0,0 +1,46 @@
+package com.escueladigital.dao;
+
+import com.escueladigital.modelos.Producto;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+import java.util.List;
+import java.util.logging.Level;
+
+public class ProductoDAO {
+
+    private static final Logger LOG = Logger.getLogger(ProductoDAO.class.getName());
+    private static final String CONSULTA = "SELECT id_producto, nombre, cantidad, precio FROM CONSULTA_productos()";
+
+    public List<Producto> listar() {
+        List<Producto> listado = new ArrayList<>();
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            con = DAO.conectar();
+            ps = con.prepareStatement(CONSULTA);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                listado.add(convertir(rs));
+            }
+        } catch (SQLException sqle) {
+            LOG.log(Level.SEVERE, "Error en ProductoDAO: {0}", sqle.getMessage());
+        } finally {
+            DAO.cerrar(con, ps, rs);
+        }
+        return listado;
+    }
+    
+    private Producto convertir(ResultSet rs) throws SQLException {
+        Producto producto = new Producto();
+        producto.id_producto = rs.getShort("id_producto");
+        producto.nombre = rs.getString("nombre");
+        producto.cantidad = rs.getShort("cantidad");
+        producto.precio = rs.getShort("precio");
+        return producto;
+    }
+}
