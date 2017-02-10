65e6a160c07343b6686057a988e73419bf43b21f
DESKTOP-H39OAJ3\danielpumayauli <danielpumayauli@gmail.com>
2017-02-10 18:56:14 -0500

    Update

--- test/com/ed/test/PruebaConexion.java
+++ test/com/ed/test/PruebaConexion.java
@@ -1,12 +1,12 @@
 package com.ed.test;
 
-import com.escueladigital.dao.UsuarioDAO;
-import com.escueladigital.modelos.Usuario;
+import com.edproj.dao.UsuarioDAO;
+import com.edproj.modelos.Usuario;
 
 public class PruebaConexion {
     public static void main(String[] args) {
         UsuarioDAO dao = new UsuarioDAO();
-        Usuario usuario = dao.autenticar("alozada", "Clave123+");
+        Usuario usuario = dao.autenticar("dpumayauli", "Clave123+");
         System.out.println("Nombre: "+usuario.nombre);
         System.out.println("Perfil: "+usuario.perfil);
     }
